75e6c838764f604be7aa92079e223f8708228a24
s1122255 <s1122255@student.hsleiden.nl>
2022-05-02 16:38:20 +0200

    css stijl van de tabel en creatie pagina aangepast

--- resources/views/tabel/timetable.blade.php
+++ resources/views/tabel/timetable.blade.php
@@ -8,8 +8,17 @@
     <ul class="u-grid-12 u-grid-gap-2">
         <h2 class="gridCard__heading">Beschikbare tijden</h2><br>
         <button class="tabelCard__button " onclick="window.location.href='/tabel/create'">Tijd reserveren</button>
+        <br>
+        <li class="u-grid-12"></li>
+        <li class="u-grid-12"></li>
+        <li class="u-grid-12"></li>
+        <li class="u-grid-12"></li>
+        <li class="u-grid-12"></li>
+        <li class="u-grid-12"></li>
+        <li class="u-grid-12"></li>
         @foreach($tijden as $tijd)
-        <li class="" data-kind-of-tabel={{$tijd->time}}>
+        <br>
+        <li class="u-grid-12 tabel" data-kind-of-tabel={{$tijd->time}}>
             <h2 class="gridCard__heading"> {{$tijd->time}} {{$tijd->telefoonnummer}} </h2>
         </li>
         @endforeach
