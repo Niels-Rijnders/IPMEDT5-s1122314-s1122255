2b58b63058b92d1452d105f75956d370718e88b5
s1122255 <s1122255@student.hsleiden.nl>
2022-05-02 11:42:21 +0200

    alles waar sushi stond vervangen met tabel

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,16 +15,6 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             ImagesTableSeeder::class,
-            // KindOfSushiTableSeeder::class,
-            // SushiMakiTableSeeder::class,
-            // SushiNigiriTableSeeder::class,
-            // SushiSashimiTableSeeder::class,
-            // SushiTemakiTableSeeder::class,
-            // SushiUramakiTableSeeder::class,
-            // KindOfDrinksTableSeeder::class,
-            // BeerTableSeeder::class,
-            // WineTableSeeder::class,
-            // SakeTableSeeder::class,
             RolesTableSeeder::class,
             UsersTableSeeder::class,
             TimetableSeeder::class,
