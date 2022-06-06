8a5e0c2bd7a2dec4e9e12ffb7323e745088724da
s1122255 <s1122255@student.hsleiden.nl>
2022-06-06 20:23:10 +0200

    comments toegevoegd aan de arduino code

--- opdracht_2.ino
+++ opdracht_2.ino
@@ -39,7 +39,9 @@ void loop() {
     data = Serial.read();
   }
 
-
+  /*hier word gechecked of er data binnenkomt van main.py. als main.py namelijk een lege plek heeft dan word l1 
+  geschrevn en dat word hier opgevangen. in het geval van l1 oftewel een lege plek dan gaat het eene ledje branden
+  en de andere niet en als er een 0 binnenkomt dan worden de ledjes omgedraait om aan te geven dat die vol is. */
   switch (data) {
     case 'l':
       object = 'l';
@@ -63,7 +65,8 @@ void loop() {
       break;
 
   }
-
+/*dit was mijn idee om te checken welk ledje wanneer moet branden maar omdat ik geen toegang had tot de hardware
+kon ik mijn deze code niet testen en debuggen dus heeft Niels Lusink de code wat aangepast zodat het voor hem werkte */
  /* if (Raan == true) {
     digitalWrite(LED, HIGH);
     digitalWrite(LEDG, LOW);
@@ -72,17 +75,19 @@ void loop() {
     digitalWrite(LEDG, HIGH);
   }
   */
-
+/*deze code zorgt ervoor dat je niet hoger dan 1800 of lager dan 900 kan, hier zou ik bijvoorbeeld willen zorgen 
+dat als je bij 1800 nog hoger gaat dat je dan bij 900 uitkomt maar omdat ik de code zelf niet kan testen 
+blijf ik er maar vanaf */
   display.print(num);
   // increment 'num'
-  if (num > 1700) {
-    num = 1700;
+  if (num > 1800) {
+    num = 1800;
   }
   if (num < 900) {
     num = 900;
   }
 
-
+/*hier worden de knoppen uitgelezen en word er 5 bij of afgetelt bij num afhankelijk van welke knop is ingedrukt */
   if ( digitalRead(bt_dn) == HIGH)
   { // if the DN button is presses
     num -= 5;
@@ -99,6 +104,12 @@ void loop() {
     Serial.println(num);
     delay(400);
   }
+
+  /*en nu komen we bij de reden voor mijn verboze comments, deze travesty. Ik hoef hier niet uit te leggen hoe dit 
+  werkt want het spreekt voor zich maar de manier waarop het gedaan is is wel een beetje jammer. Eerst had ik hier
+  een zelfde soort if loop getypt als bij de timetableseeder maar wederom omdat ik de code zelf niet kon testen
+  heeft Niels Lusink het op zijn eigen manier gedaan en ookal typ ik dit allemaal, het werkt wel tenminste 
+  en omdat het een klein programma is is er geen hele erge slowdown dus voor nu is dit goed genoeg. */
   // decrement 'num'
   if(num == 960){
      num = 1000;}
@@ -116,6 +127,8 @@ void loop() {
      num = 1600;}
      if(num == 1660){
      num = 1700;}
+     if(num == 1760){
+       num = 1800;}
 
      
      if (num == 995) {
@@ -142,6 +155,9 @@ void loop() {
      }
      if (num == 1695) {
      num = 1655;
-     } 
+     }
+     if (num == 1795) {
+       num == 1755;
+     }
 
 }
